7869d109da831699eb2d7f1a1164829719d7b44b
Douglas Gomes <41962844+DogdyZero@users.noreply.github.com>
2018-12-04 19:41:44 -0200

    Update RG.java

--- src/br/com/sistema/RG.java
+++ src/br/com/sistema/RG.java
@@ -5,15 +5,33 @@ import br.com.sistema.util.Tipo;
 
 @Tipo("cliente")
 public class RG {
-	@Atributo ("numero")
-	private String numero;
+	
+    private String numero;
+    private String orgaoExpeditor;
+    private Estado estadoExpeditor;
 
-	public String getNumero() {
-		return numero;
-	}
+    public String getNumero() {
+        return numero;
+    }
 
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getOrgaoExpeditor() {
+        return orgaoExpeditor;
+    }
+
+    public void setOrgaoExpeditor(String orgaoExpeditor) {
+        this.orgaoExpeditor = orgaoExpeditor;
+    }
+
+    public Estado getEstadoExpeditor() {
+        return estadoExpeditor;
+    }
+
+    public void setEstadoExpeditor(Estado estadoExpeditor) {
+        this.estadoExpeditor = estadoExpeditor;
+    }
 	
 }
