3bad5ddb1a44945fba6289e1236d200a985f1370
Douglas Gomes <41962844+DogdyZero@users.noreply.github.com>
2018-12-05 15:41:29 -0200

    Update Jeyson2.java

--- src/br/com/sistema/Jeyson2.java
+++ src/br/com/sistema/Jeyson2.java
@@ -19,7 +19,8 @@ public class Jeyson2 {
 		try {
 			Class<?> classe = Class.forName(nmClasse);
 			verificarSubClasse(classe);
-			
+			sb.append("}");
+
 			
 		} catch (ClassNotFoundException | IllegalArgumentException | IllegalAccessException e) {
 
@@ -27,20 +28,20 @@ public class Jeyson2 {
 		}
 		
 		
-		return null;
+		return sb.toString();
 	}
 	private void verificarSubClasse(Class classe) throws IllegalArgumentException, IllegalAccessException, ClassNotFoundException {
 		
 		
 		Field[] f = classe.getDeclaredFields();
-		System.out.println("{");
+		sb.append("{");
 		for(Field campos:f) {
 			campos.setAccessible(true);
 			this.valorAtributo = campos.get(obj);
 			this.tipoAtributo = campos.getType();
-			
+			this.nomeAtributo = campos.getName();
 			if(!verificarTiposCompostos(f))
-				System.out.println("\""+campos.getName()+"\" :"+"\""+campos.get(obj)+"\",");
+			sb.append("\""+campos.getName()+"\" :"+"\""+campos.get(obj)+"\",");
 			
 		}
 		verificarSuperClasse(classe);
@@ -48,35 +49,41 @@ public class Jeyson2 {
 	
 	private void verificarSuperClasse(Class classe) throws IllegalArgumentException, IllegalAccessException, ClassNotFoundException {
 		Field[] f = classe.getSuperclass().getDeclaredFields();
+		
+
 		for(Field campos:f) {
 			campos.setAccessible(true);
 			this.valorAtributo = campos.get(obj);
 			this.tipoAtributo = campos.getType();
+			this.nomeAtributo = campos.getName();
 			if(!verificarTiposCompostos(f))
-				System.out.println("\""+campos.getName()+"\" :"+"\""+campos.get(obj)+"\",");
+				sb.append("\""+campos.getName()+"\" :"+"\""+campos.get(obj)+"\",");
 
 		}
-		sb.append("");
-		sb.append("}");
-		
+
 	}
 	private boolean verificarTiposCompostos(Field[] campos) throws IllegalArgumentException, IllegalAccessException, ClassNotFoundException {
 		if(this.tipoAtributo.toString().contains("[L")) {
 			verificarArray(campos);
+			sb = sb.delete(sb.toString().length()-1, sb.toString().length());
+
+			sb.append("]");
 			return true;
 		}
 			
-		else if(this.tipoAtributo.toString().equals("class br.com.sistema.Estado")) {
+		else if(this.tipoAtributo.toString().equals("class Estado")) {
 			verificarObjeto(campos);
+			sb = sb.delete(sb.toString().length()-1, sb.toString().length());
+			sb.append("},");
 			return true;
 		}
 			
 		return false;
 	}
 	
-		private void verificarObjeto(Field[] campos) throws IllegalArgumentException, IllegalAccessException, ClassNotFoundException {
+	private void verificarObjeto(Field[] campos) throws IllegalArgumentException, IllegalAccessException, ClassNotFoundException {
 		Class<?> classeFilha =(Class<?>)this.tipoAtributo;
-		sb.append(":{");
+		sb.append("\""+this.nomeAtributo+"\" : {");
 		Field[] ff = classeFilha.getDeclaredFields();
 		obj = this.valorAtributo;
 		for(Field campoFilha: ff ) {
@@ -85,18 +92,20 @@ public class Jeyson2 {
 			campoFilha.get(this.valorAtributo);
 			this.tipoAtributo = campoFilha.getType();
 			if(!verificarTiposCompostos(ff))
-				System.out.println("\""+campoFilha.getName()+"\" :"+"\""+campoFilha.get(obj)+"\",");
-
+			sb.append("\""+campoFilha.getName()+"\" :"+"\""+campoFilha.get(obj)+"\",");
 		}
 	}
+
 	public void verificarArray(Field[] campos) throws IllegalArgumentException, IllegalAccessException, ClassNotFoundException {
-		
+	    
+	    sb.append("\""+this.nomeAtributo+"\" : [ ");
 		for (Object obj : (Object[]) this.valorAtributo)
 		{
+		    sb.append("{");
 			if (obj== null)
 				break;
 			Class classeFilha;
-			sb.append(":{");
+			
 			classeFilha = Class.forName(obj.getClass().getCanonicalName());
 			Field[] ff = classeFilha.getDeclaredFields();
 
@@ -106,13 +115,13 @@ public class Jeyson2 {
 				this.valorAtributo = campoFilha.get(obj);
 				this.tipoAtributo = campoFilha.getType();
 				if(!verificarTiposCompostos(ff))
-					System.out.println("\""+campoFilha.getName()+"\" :"+"\""+campoFilha.get(obj)+"\",");
-
+					sb.append("\""+campoFilha.getName()+"\" :"+"\""+campoFilha.get(obj)+"\",");
 			}
+			sb = sb.delete(sb.toString().length()-1, sb.toString().length());
+			sb.append("},");
 		}
+		sb = sb.delete(sb.toString().length()-1, sb.toString().length());
+		sb.append("}");
 			
-		
-		
-		
 	}
 }
